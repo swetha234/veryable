1a17d389b80546e5e42314908777cec79ad99e47
swetha234 <hgutt001@odu.edu>
2021-06-03 16:58:37 -0400

    completed

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
 import './App.scss';
-import './responsive-topography.scss';
 import Accordian from './Accordian/Accordian';
 import './Accordian/Accordian.scss';
 import UserLogo from './group.svg';
 import Users from './users.json';
-<link rel="stylesheet" href= "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></link>
 
-function App() {
-  return (
 
-    <div className="container">
-
-      {/* Header of the Page */}
-      <div class="nav-bar">
-        <div class="user-logo">
-            <img src={UserLogo} alt="User Logo"></img>
-            <h1 class="h4">Users</h1>
-        </div>
+const App = () => (
+  <div className="container">
+    {/* Header of the Page */}
+    <div className="nav-bar">
+      <div className="user-logo">
+          <img src={UserLogo} alt="User Logo"></img>
+          <h1 className="h4">Users</h1>
       </div>
-
-      {/* Body of the Page */}
-     
-        { Users.map(post => 
-  
-            <div >
-              
-              <div class="listOfDetails">
-
-               <Accordian
-                firstname={post.firstName} lastname={post.lastName} role={post.role} email={post.email}  street={post.street}  city={post.city} 
-                state={post.state} zip={post.zip} phone={post.phone} createdat={post.createdAt} lastlogin={post.lastLoggedIn} 
-               
-               ></Accordian>
-                 
-              </div>
-            </div>
-            
-          
-        ) }
     </div>
-  );
-}
+
+    {/* Body of the Page */}
+    { Users.map(user => 
+      <div >
+        <Accordian
+          firstname = {user.firstName} 
+          lastname  = {user.lastName} 
+          role = {user.role}
+          email = {user.email}  
+          street = {user.street}  
+          city = {user.city} 
+          state = {user.state} 
+          zip = {user.zip} 
+          phone = {user.phone} 
+          createdat = {user.createdAt} 
+          lastlogin = {user.lastLoggedIn} 
+        /> 
+      </div>
+    )}
+  </div>
+);
 
 export default App;
